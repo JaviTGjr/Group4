b6ed9232689bae81ad9d7185a486149d410235cb
ercolani <90383208+ercolani@users.noreply.github.com>
2021-11-09 15:01:59 +0100

    Main Menu
    Main Menu and waypoint fixes

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -28,13 +28,13 @@ public class Enemy : MonoBehaviour
     void GetNextWaypoint()
     {
         
-        if (waypointindex > Waypoints.current.transform.childCount - 1)
+        if (waypointindex > Waypoints.current.transform.childCount)
         {
 
             Destroy(gameObject);
             return;
         }
-        else if(waypointindex == Waypoints.current.transform.childCount - 1)
+        else if(waypointindex == Waypoints.current.transform.childCount)
         {
             target = new Vector3(Waypoints.current.LastWaypoint().x, this.transform.position.y, Waypoints.current.LastWaypoint().z);
             waypointindex++;
